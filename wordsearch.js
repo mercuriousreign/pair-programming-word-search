e2738395c18dda45e06f9ee3b48348485aa8aee9
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-18 16:57:13 -0500

    debuged code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
+const transpose = function(matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArr[i] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newArr[i].push(matrix[j][i]);
+    }
+  }
+  return newArr;
+};
+
+
+
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    let horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    horizontalJoin = transpose(letters).map(ls => ls.join(''))
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false;
+
 }
 
 module.exports = wordSearch
